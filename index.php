6e9af46362d4365ffcf3963b9469cbbc66f39b6f
Carl Dahlgren <83665107+carldahlgren@users.noreply.github.com>
2024-08-20 22:40:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-header("location:./views/start.php") 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect</title>
+    <script type="text/javascript">
+        window.location.href = "./views/start.php";
+    </script>
+</head>
+<body>
+    <!-- Fallback message if JavaScript is disabled -->
+    <p>If you are not redirected automatically, follow this <a href="./views/start.php">link to the start page</a>.</p>
+</body>
+</html>
